d44c815d8b326b7f879b20efcf3896cdc56216cf
NikCha0s <94637299+NikCha0s@users.noreply.github.com>
2022-04-30 14:52:16 +0200

    Initial Commit

---
+++ www/script.js
@@ -0,0 +1,36 @@
+/*
+dtsizetxt primo input
+dlspeedtxt secondo input
+calcola bottone
+internetspeed seconda combobox
+datasize prima combobox
+*/
+
+function ResetData(){
+    document.getElementById("datasize").selectedIndex = 2;
+    document.getElementById("internetspeed").selectedIndex = 5;
+    document.getElementById("dtsizetxt").value = "";
+    document.getElementById("dlspeedtxt").value = "";
+}
+
+function Calcola(){
+    var dataSize = document.getElementById("dtsizetxt");
+    var downloadSpeed = document.getElementById("dlspeedtxt");
+    var dataSizeValue = dataSize.value.replace(",",".");
+    var downloadSpeed
+    if(dataSize.value == "" || downloadSpeed.value == "" || dataSize.value == "." || downloadSpeed.value == "." || dataSize == "," || downloadSpeed == ",") return;
+    var QuantitaDatiDaScaricare = parseFloat(dataSize.value);
+}
+
+function ElaboraTempo(tempo) {
+    var giorni = Math.trunc(tempo / 86400);
+    var secondi = (int)(tempo % 86400); //86400 = (24*3600), pre-calcolato per diminuire i calcoli, usando la conversione esplicita con (int) il valore viene automaticamente troncato
+
+    var ore = (secondi / 3600);
+    secondi %= 3600;
+
+    var minuti = (secondi / 60);
+    secondi %= 60;
+
+    return `${giorni} giorni ${ore} ore ${minuti} minuti ${secondi}`;
+}
