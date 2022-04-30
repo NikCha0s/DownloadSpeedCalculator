456e44e37be4f2dd87e79d623ea0306e2abb337f
NikCha0s <94637299+NikCha0s@users.noreply.github.com>
2022-04-30 18:14:52 +0200

    Divided javascript file by their functionality

---
+++ www/GUI.js
@@ -0,0 +1,10 @@
+function percentualeDi(numeroIntero, percentuale) {
+    return Math.round(numeroIntero / 100 * percentuale);
+}
+
+function fixDimension() {
+    var body = document.getElementsByTagName("body")[0];
+    body.style.height = "auto"
+    body.classList.remove(".firstbody");
+    body.style.height = (body.clientHeight + percentualeDi(body.clientHeight, 3)).toString() + "px";
+}
--- www/app.js
+++ www/app.js
@@ -6,19 +6,19 @@ internetspeed seconda combobox
 datasize prima combobox
 */
 
-function ResetData(){
+function ResetData() {
     document.getElementById("datasize").selectedIndex = 2;
     document.getElementById("internetspeed").selectedIndex = 5;
     document.getElementById("dtsizetxt").value = "";
     document.getElementById("dlspeedtxt").value = "";
 }
 
-function Calcola(){
+function Calcola() {
     var dataSize = document.getElementById("dtsizetxt");
     var downloadSpeed = document.getElementById("dlspeedtxt");
-    var dataSizeValue = dataSize.value.replace(",",".");
-    var downloadSpeedValue = downloadSpeed.value.replace(",",".");
-    if(dataSize.value == "" || downloadSpeed.value == "" || dataSize.value == "." || downloadSpeed.value == "." || dataSize == "," || downloadSpeed == ",") return;
+    var dataSizeValue = dataSize.value.replace(",", ".");
+    var downloadSpeedValue = downloadSpeed.value.replace(",", ".");
+    if (dataSize.value == "" || downloadSpeed.value == "" || dataSize.value == "." || downloadSpeed.value == "." || dataSize == "," || downloadSpeed == ",") return;
     var QuantitaDatiDaScaricare = parseFloat(dataSize.value);
 }
 
@@ -33,14 +33,4 @@ function ElaboraTempo(tempo) {
     secondi %= 60;
 
     return `${giorni} giorni ${ore} ore ${minuti} minuti ${secondi}`;
-}
-
-function percentualeDi(numeroIntero, percentuale){
-    return Math.round(numeroIntero/100*percentuale);
-}
-
-function fixDimension(){
-    var body = document.getElementsByTagName("body")[0];
-    body.classList.remove(".firstbody");
-    body.style.height = (body.clientHeight + percentualeDi(body.clientHeight,3)).toString() + "px";
 }
